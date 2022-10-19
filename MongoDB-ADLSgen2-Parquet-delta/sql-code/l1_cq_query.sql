519ee9a26743610032e80dda71145433ae80b932
Sriram @Striim <103606614+sriram-striim@users.noreply.github.com>
2022-10-19 15:17:54 -0500

    mongodb recipe query

---
+++ MongoDB-ADLSgen2-Parquet-delta/sql-code/l1_cq_query.sql
@@ -0,0 +1,13 @@
+SELECT
+TO_STRING(data.get("_id")) as id,
+TO_STRING(data.get("name")) as name,
+TO_STRING(data.get("property_type")) as property_type,
+TO_STRING(data.get("room_type")) as room_type,
+TO_STRING(data.get("bed_type")) as bed_type,
+TO_STRING(data.get("minimum_nights")) as minimum_nights,
+TO_STRING(data.get("cancellation_policy")) as cancellation_policy,
+TO_STRING(data.get("accommodates")) as accommodates,
+TO_STRING(data.get("bedrooms")) as no_of_bedrooms,
+TO_STRING(data.get("beds")) as no_of_beds,
+TO_STRING(data.get("number_of_reviews")) as no_of_reviews
+FROM l1_stream l
